636707d82735a63c26de49005bde0732ec5932f8
Lucas P Oliveira <lucasp.oliveira122@gmail.com>
2023-11-27 21:41:08 -0300

    versão final.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 class Program
 {
@@ -8,24 +9,9 @@ class Program
     {
         string textoCifrado = File.ReadAllText("provinhaBarbadinha.txt");
         string textoDecifrado = decifrar(textoCifrado);
-        textoDecifrado = textoCifrado.Replace("@", "\n");
+        textoDecifrado = textoDecifrado.Replace("@", "\n");
 
         string[] palindromos = palindromo(textoDecifrado);
-        foreach (var item in palindromos)
-        {
-            switch (item)
-            {
-                case "gloriosa":
-                    textoDecifrado = textoDecifrado.Replace(item, "gloriosa");
-                    break;
-                case "bondade":
-                    textoDecifrado = textoDecifrado.Replace(item, "bondade");
-                    break;
-                case "passam":
-                    textoDecifrado = textoDecifrado.Replace(item, "passam");
-                    break;
-            }
-        }
 
         Console.WriteLine("Conteúdo do texto cifrado: \n" + textoCifrado);
         Console.WriteLine("\nPalíndromos encontrados: ");
@@ -38,23 +24,28 @@ class Program
         Console.WriteLine("\nNúmero de caracteres do texto decifrado: " + textoDecifrado.Length);
         Console.WriteLine("\nQuantidade de palavras no texto decifrado: " + textoDecifrado.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length);
         Console.WriteLine("\nTexto decifrado em maiúsculas: \n" + textoDecifrado.ToUpper());
-
     }
 
     static string decifrar(string textoCifrado)
     {
-        char[] caracteres = textoCifrado.ToCharArray();
-        for (int i = 0; i < caracteres.Length; i++)
+        char[] caracteresCifrados = textoCifrado.ToCharArray();
+        for (int i = 0; i < caracteresCifrados.Length; i++)
         {
-            caracteres[i] = (char)(i % 5 == 0 ? caracteres[i] - 8 : caracteres[i] - 16);
+            int chave = (i % 5 == 0) ? 8 : 16;
+            caracteresCifrados[i] = (char)(caracteresCifrados[i] - chave);
         }
-        return new string(caracteres);
+        return new string(caracteresCifrados);
     }
-
     static string[] palindromo(string texto)
     {
         return texto.Split(' ', StringSplitOptions.RemoveEmptyEntries)
-               .Where(palavra => palavra == new string(palavra.Reverse().ToArray()))
-               .ToArray();
+            .Where(palavra =>
+            {
+                char[] caracteres = palavra.ToCharArray();
+                Array.Reverse(caracteres);
+                return palavra.Equals(new string(caracteres), StringComparison.OrdinalIgnoreCase);
+            })
+            .ToArray();
     }
+
 }
